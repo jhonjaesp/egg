066a76ed80d102a184761b1562abab8b68a42e2f
Jhon Espinal <jhon122309@gmail.com>
2023-03-24 01:09:36 -0500

    primmer commit

---
+++ src/ensayos.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jjchi
+ */
+public class ensayos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+       Scanner leer=new Scanner(System.in);
+       String nombre;
+       System.out.println("ingresa tu nombre"); 
+       
+       nombre = leer.next();
+       
+       System.out.println("hola" + nombre + "bienvenido al curso");
+       
+    }
+    
+}
